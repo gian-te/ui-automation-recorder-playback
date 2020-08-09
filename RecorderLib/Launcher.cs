d34eb95b878512bcb0e1d57304ef1c65ea3d56c8
gian-te <gian.te@outlook.com>
2020-08-10 03:40:41 +0800

    initial commit for user action recording

---
+++ RecorderLib/Launcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+
+
+namespace RecorderLib
+{
+    class Launcher
+    {
+        public static ChromeDriver LaunchChrome()
+        {
+            return new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory);
+        }
+
+        public static void ExitBrowser(ChromeDriver driver)
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RecorderLib;
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Step> users = new ObservableCollection<Step>();
+        public MainWindow()
+        {
+            try
+            {
+                InitializeComponent();
+                dgUsers.ItemsSource = users;
+                // [GT] https://docs.microsoft.com/en-us/dotnet/api/system.componentmodel.backgroundworker?view=netframework-4.7.2
+                BackgroundWorker bgw = new BackgroundWorker();
+                bgw.DoWork +=
+                new DoWorkEventHandler(backgroundWorker1_DoWork);
+                bgw.RunWorkerAsync();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //Application.Current.Dispatcher.Invoke(() =>
+            //  {
+            Recorder recorder = new Recorder();
+            recorder.LaunchBrowser();
+            recorder.GoToUrl(@"http://www.facebook.com");
+            while (true)
+            {
+                try
+                {
+                    Step obj = recorder.AttachListener(users.Count);
+
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        users.Add(obj);
+                    });
+                }
+                catch
+                {
+
+                }
+
+            }
+
+        }
+
+
+
+    }
+}
